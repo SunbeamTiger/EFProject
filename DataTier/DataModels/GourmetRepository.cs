e1c0622b26a7a9f21d23d8799a02050d630f7189
dan.hagen <dan.hagen@centralia.edu>
2024-02-28 09:55:10 -0800

    Add project files.

---
+++ DataTier/DataModels/GourmetRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTier.Context;
+using DataTier.DataModels;
+
+namespace DataTier.DataModels
+{
+    public class GourmetRepository
+    {
+        private GourmetContext _context;
+        public GourmetRepository() 
+        { 
+            _context = new GourmetContext();
+        }  
+        public List <Customer> GetCustomers() 
+        { 
+            return _context.Customers.ToList();  
+        }
+    }
+}
---
+++ PresentationTier/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataTier.Context;
+using DataTier.DataModels;
+namespace PresentationTier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GourmetRepository repository = new GourmetRepository();
+            List<Customer> myCustomers = repository.GetCustomers();
+         
+            foreach ( var customer in myCustomers) 
+            { 
+                Console.WriteLine( customer.LastName );  
+            }
+        }
+    }
+}
